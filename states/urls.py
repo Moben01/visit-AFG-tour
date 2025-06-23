b41365721c4d4fffe83110f735c32a0da2a89c58
Masi_Sediqi <154883375+Masi-Sediqi@users.noreply.github.com>
2025-06-23 17:23:58 +0430

    other province completed

--- states/urls.py
+++ states/urls.py
@@ -7,4 +7,12 @@ urlpatterns = [
     path('kabul', views.kabul, name='kabul'),
     path('parwan', views.parwan, name='parwan'),
     path('maidan_wardak', views.maidan_wardak, name='maidan_wardak'),
+    path('bamyan', views.bamyan, name='bamyan'),
+    path('logar', views.logar, name='logar'),
+    path('kapisa', views.kapisa, name='kapisa'),
+    path('panjshir', views.panjshir, name='panjshir'),
+    path('daikundi', views.daikundi, name='daikundi'),
+    path('ghazni', views.ghazni, name='ghazni'),
+    path('paktika', views.paktika, name='paktika'),
+    path('khost', views.khost, name='khost'),
 ]
--- states/views.py
+++ states/views.py
@@ -10,3 +10,27 @@ def parwan(request):
 
 def maidan_wardak(request):
     return render(request, 'states/maidan_wardak.html')
+
+def bamyan(request):
+    return render(request, 'states/bamyan.html')
+
+def logar(request):
+    return render(request, 'states/logar.html')
+
+def kapisa(request):
+    return render(request, 'states/kapisa.html')
+
+def panjshir(request):
+    return render(request, 'states/panjshir.html')
+
+def daikundi(request):
+    return render(request, 'states/daikundi.html')
+
+def ghazni(request):
+    return render(request, 'states/ghazni.html')
+
+def paktika(request):
+    return render(request, 'states/paktika.html')
+
+def khost(request):
+    return render(request, 'states/khost.html')
