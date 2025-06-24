989bbd3c2f64917ce6508e4e547ef58712848ffe
Hotak <enayatullahh76@gmail.com>
2025-06-24 14:05:34 +0430

    12 province complate

--- states/urls.py
+++ states/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
 from .import views
 
+
 app_name = 'home'
 
 urlpatterns = [
-    path('kabul', views.kabul, name='kabul')
+    path('kabul', views.kabul, name='kabul'),
+    path('balkh', views.balkh, name='balkh'),
+    path('samangan', views.samangan, name='samangan'),
+    path('jawzjan', views.jawzjan, name='jawzjan'),
+    path('faryab', views.faryab, name='faryab'),
+    path('SarePol', views.SarePol, name='SarePol'),
+    path('Baghlan', views.Baghlan, name='Baghlan'),
+    path('Kunduz', views.Kunduz, name='Kunduz'),
+    path('Takhar', views.Takhar, name='Takhar'),
+    path('Badakhshan', views.Badakhshan, name='Badakhshan')
 ]
