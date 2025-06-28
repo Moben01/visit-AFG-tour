071c921c81bd8e77e119ae6aa07c1ade7c5842c7
shafi <shafifazli73@gmail.com>
2025-06-28 16:42:26 +0430

    wheather api config

--- things_to_do/views.py
+++ things_to_do/views.py
@@ -1,11 +1,55 @@
-from django.shortcuts import render
 
-# Create your views here.
+import requests
+from django.shortcuts import render
+import os
+from dotenv import load_dotenv
+load_dotenv()
 import requests
 from django.shortcuts import render
 
 def Art_and_Culture(request):
+    api_key = "2c8f25d1c7a10cb132adfa1884766a2e"
 
-    return render(request, 'things_to_do/Art_and_Culture.html')
+    # City names with country code Afghanistan (AF)
+    cities = [
+        "Kabul,AF",
+        "Herat,AF",
+        "Kandahar,AF",
+        
+        "Jalalabad,AF",
+        "Kunduz,AF",
+        "Ghazni,AF",
+        "Balkh,AF",
+        "Puli Khumri,AF",
+        "Taloqan,AF",
+        "Logar,AF",
+        "Nangarhar,AF",
+        "Badakhshan,AF",
+        "Farah,AF",
+        "Baghlan,AF",
+    ]
 
+    weather_data = {}
 
+    for city in cities:
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}"
+        print(f"Request URL: {url}")
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            data = response.json()
+            city_name = city.split(",")[0]  # Remove country code for display
+            weather_data[city_name] = {
+                "temp": data["main"]["temp"],
+                "description": data["weather"][0]["description"].title(),
+                "icon": data["weather"][0]["icon"],
+            }
+        else:
+            city_name = city.split(",")[0]
+            weather_data[city_name] = {
+                "temp": "N/A",
+                "description": "Data not available",
+                "icon": None,
+            }
+
+    return render(request, 'things_to_do/Art_and_Culture.html', {'weather_data': weather_data})
