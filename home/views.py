4a72e679cf137733a98dc79214b99faa1f570333
Hotak <enayatullahh76@gmail.com>
2025-07-01 15:39:53 +0430

    num.. and emil admin registaer

--- home/views.py
+++ home/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
-from django.utils.translation import get_language
+from django.utils.translation import *
 from tour.models import *
 
 # Create your views here.
-
 def home_view(request):
     get_tour_categories = TourCategory.objects.all()
+    get_main_things = Main_things.objects.last()
+    user_id = request.user.id
+    find_user = User.objects.get(id=user_id)
+    find_user_favorite = User_favorite_tour.objects.filter(user=find_user, favorite=True).count()
+
     language_code = get_language()
 
     if language_code == 'fa' or language_code == "ar":
             # Do something for right-to-left languages
         return render(request, 'RTL/index.html')
     else:
-        return render(request, 'index.html', {'get_tour_categories':get_tour_categories})
-
-def custom_404_view(request, exception):
-    return render(request, '404.html', status=404)
+        return render(request, 'index.html', {'get_tour_categories':get_tour_categories, 'find_user_favorite':find_user_favorite, 'get_main_things':get_main_things})
