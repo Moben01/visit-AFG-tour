e36461c5838d0481c4edac8d2770ecf6c5c3d4cd
Moben01 <54992059+Moben01@users.noreply.github.com>
2025-07-09 12:05:58 +0430

    project updated

--- visit_afg_core/settings.py
+++ visit_afg_core/settings.py
@@ -98,12 +98,26 @@ WSGI_APPLICATION = 'visit_afg_core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+# PostgreSQL Database Configuration
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'visitafgtoursdb',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',  # Replace with your PostgreSQL server's address if necessary
+        'PORT': '5432',          # Leave empty to use the default PostgreSQL port (usually 5432)
     }
 }
+
 
 
 # Password validation
