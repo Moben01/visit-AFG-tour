3485d2be8cf1282c7b2d1b7b489e8f25b3251c55
bilal-nr <Bilal.noori091@gmail.com>
2025-07-15 10:24:14 +0430

    Add Best Places for Visit models and admin integration
    Introduces Best_places_for_visit and Best_places_for_visit_images models with province selection and image support. Updates admin to allow inline image management for places and registers the new models. Also adds 'things_to_do' to INSTALLED_APPS in settings.

--- things_to_do/models.py
+++ things_to_do/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from multiselectfield import MultiSelectField
 
-# Create your models here.
+class Best_places_for_visit(models.Model):
+    PROVINCE_CHOICES = [
+        ('Badakhshan', 'Badakhshan'), ('Badghis', 'Badghis'), ('Baghlan', 'Baghlan'),
+        ('Balkh', 'Balkh'), ('Bamyan', 'Bamyan'), ('Daykundi', 'Daykundi'),
+        ('Farah', 'Farah'), ('Faryab', 'Faryab'), ('Ghazni', 'Ghazni'),
+        ('Ghor', 'Ghor'), ('Helmand', 'Helmand'), ('Herat', 'Herat'),
+        ('Jowzjan', 'Jowzjan'), ('Kabul', 'Kabul'), ('Kandahar', 'Kandahar'),
+        ('Kapisa', 'Kapisa'), ('Khost', 'Khost'), ('Kunar', 'Kunar'),
+        ('Kunduz', 'Kunduz'), ('Laghman', 'Laghman'), ('Logar', 'Logar'),
+        ('Nangarhar', 'Nangarhar'), ('Nimroz', 'Nimroz'), ('Nuristan', 'Nuristan'),
+        ('Paktia', 'Paktia'), ('Paktika', 'Paktika'), ('Panjshir', 'Panjshir'),
+        ('Parwan', 'Parwan'), ('Samangan', 'Samangan'), ('Sar-e Pol', 'Sar-e Pol'),
+        ('Takhar', 'Takhar'), ('Urozgan', 'Urozgan'), ('Wardak', 'Wardak'),
+        ('Zabul', 'Zabul'),
+    ]
+
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='place_images/')
+    description = models.TextField()  # 📌 added for details
+    location = models.CharField(max_length=200)
+    provinces = MultiSelectField(choices=PROVINCE_CHOICES, max_choices=34, max_length=400)
+
+    def __str__(self):
+        return self.title
+
+
+class Best_places_for_visit_images(models.Model):
+    place = models.ForeignKey(Best_places_for_visit, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='place_images/more/')
+    caption = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return f"Image of {self.place.title}"
