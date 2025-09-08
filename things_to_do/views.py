4574f98b5a0a35398c830bb85e1c95952773e7df
shafiqalandari <shafifazli73@gmail.com>
2025-09-08 12:03:39 +0430

    photo changes

--- things_to_do/views.py
+++ things_to_do/views.py
@@ -13,56 +13,8 @@ from django.shortcuts import render
 
 def Art_and_Culture(request):
     get_tour_categories = TourCategory.objects.all()
-    api_key = "2c8f25d1c7a10cb132adfa1884766a2e"
 
-    cities = [
-        "Kabul,AF",
-        "Herat,AF",
-        "Kandahar,AF",
-        "Jalalabad,AF",
-        "Kunduz,AF",
-        "Ghazni,AF",
-        "Balkh,AF",
-        "Puli Khumri,AF",
-        "Taloqan,AF",
-        "Logar,AF",
-        "Nangarhar,AF",
-        "Badakhshan,AF",
-        "Farah,AF",
-        "Baghlan,AF",
-    ]
-
-    weather_data = {}
-
-    for city in cities:
-        try:
-            url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}"
-            response = requests.get(url, timeout=5)  # timeout added to avoid hanging
-            city_name = city.split(",")[0]
-
-            if response.status_code == 200:
-                data = response.json()
-                weather_data[city_name] = {
-                    "temp": data["main"]["temp"],
-                    "description": data["weather"][0]["description"].title(),
-                    "icon": data["weather"][0]["icon"],
-                }
-            else:
-                weather_data[city_name] = {
-                    "temp": "N/A",
-                    "description": "Data not available",
-                    "icon": None,
-                }
-        except Exception as e:
-            # Handle no internet or timeout
-            city_name = city.split(",")[0]
-            weather_data[city_name] = {
-                "temp": "N/A",
-                "description": "Data not available",
-                "icon": None,
-            }
-
-    return render(request, 'things_to_do/Art_and_Culture.html', {'weather_data': weather_data, 'get_tour_categories':get_tour_categories})
+    return render(request, 'things_to_do/Art_and_Culture.html', {'get_tour_categories':get_tour_categories})
 
 
 
